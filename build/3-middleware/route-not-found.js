f8ad098f7795641403fdf3f1c38f08f9f862330f
kobi krasnoff <krasnoff.kobi@gmail.com>
2023-01-03 10:07:18 +0200

    quick fix

--- build/3-middleware/route-not-found.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var error_models_1 = require("../4-models/error-models");
-function routeNotFound(request, response, next) {
-    next(new error_models_1.RouteNotFoundError(request.originalUrl));
-}
-exports.default = routeNotFound;
